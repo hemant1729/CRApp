5f52ed8b1c5eef75ceb7cdcb12c9853d5763f00e
VinayakaGosula <50439962+VinayakaGosula@users.noreply.github.com>
2021-05-05 23:26:53 +0530

    Update views.py

--- app/admin/views.py
+++ app/admin/views.py
@@ -121,3 +121,82 @@ def admin_course(request):
 @user_passes_test(is_admin)
 def admin_test(request):
     return render(request, 'admin/test.html')
+@user_passes_test(is_admin)
+def admin_tags(request):
+    error = ''
+    context = {}
+    if request.method == 'POST': 
+        data = request.POST
+        try:
+            if 'add' in data:
+                tag_name = data['tag_name']
+                tag = Tag(tag_name=tag_name)
+                tag.insert()
+            elif 'search' in data:
+                tag_name = data['tag_name']
+                tag_list = Tag.search(tag_name)
+                tag_names = [d.tag_name for d in tag_list]
+                context['tag_names'] = tag_names
+            elif 'update' in data:
+                #delete has more priority 
+                for d in data.getlist('delete_list'):
+                    tag = Tag(d)
+                    tag.delete()
+                for u in data.getlist('update_list'):
+                    uid, uname = u.split(':')
+                    if uname in data.getlist('delete_list'):
+                        continue
+                    tag = Tag(tag_name=uname)
+                    tag.update(data.getlist('updated_names')[int(uid)])
+
+        except:
+            error = 'DB error'
+
+    context['error'] = error 
+    return render(request, 'admin/tags.html', context)
+
+@user_passes_test(is_admin)
+def admin_course_tags(request):
+    if request.GET.get('course_id'):
+        error = ''
+        context = {}
+        try:
+            course_tag = Course_tags(request.GET['course_id'])
+            tag_list = course_tag.get_tags()
+            tag_names = [d.tag_name for d in tag_list]
+            context['course_tag_names'] = tag_names
+            context['course_name'] = course_tag.get_name()
+            context['course_id'] = request.GET['course_id']
+        except:
+            error = 'DB error'
+        context['error'] = error
+        return render(request, 'admin/course_tags.html', context)
+    error = ''
+    context = {}
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+        try:
+            context['course_name'] = data['course_name']
+            context['course_id'] = data['course_id']
+            if 'search' in data:
+                tag_name = data['tag_name']
+                tag_list = Tag.search(tag_name)
+                tag_names = [d.tag_name for d in tag_list]
+                context['tag_names'] = tag_names
+            elif 'update' in data:
+                course_id = data['course_id']
+                course_tag = Course_tags(course_id)
+                for d in data.getlist('delete_list'):
+                    course_tag.remove_tag(d)
+                for a in data.getlist('add_list'):
+                    course_tag.add_tag(a)
+            course_tag = Course_tags(data['course_id'])
+            tag_list = course_tag.get_tags()
+            tag_names = [d.tag_name for d in tag_list]
+            context['course_tag_names'] = tag_names
+        except:
+            error = 'DB error'
+
+    context['error'] = error 
+    return render(request, 'admin/course_tags.html', context)
